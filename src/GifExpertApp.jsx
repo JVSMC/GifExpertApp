30257c11c69a9a373baabaf6d1f8c6d34464280a
Lap_Javi-Local <javixxmc@gmail.com>
2023-08-22 07:47:43 -0600

    --| Firts commit |--

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'// A partir de la versión 17 ya no se importa react por todos lados
+
+function GifExpertApp() {
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+        </>
+    )
+}
+
+export default GifExpertApp
