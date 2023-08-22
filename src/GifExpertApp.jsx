09fe99aab318258de235fc85f8b60d7f6ffa8786
Lap_Javi-Local <javixxmc@gmail.com>
2023-08-22 08:50:27 -0600

    --| Making a component list |--

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,31 @@
-import React from 'react'// A partir de la versión 17 ya no se importa react por todos lados
-
+import React, {useState} from 'react'// A partir de la versión 17 ya no se importa react por todos lados
+/**
+ * Estructura
+ * Título
+ * Input
+ * Listado del Gif
+ *  -> Gif items
+ */
 function GifExpertApp() {
+    const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ]);
+    console.log(categories);
+
     return (
         <>
             <h1>GifExpertApp</h1>
+            {/* CONSTRUCCIÓN DE MANERA DINAMICA */}
+            <ol>
+                { categories.map( category =>{
+                    return <li key={category}>{category}</li>
+                } ) }
+            </ol>
         </>
     )
 }
 
 export default GifExpertApp
+
+{/**
+    --| Map |--
+    
+*/}
