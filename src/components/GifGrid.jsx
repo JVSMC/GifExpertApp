6af91634d728c68455aa441c4d31544ccae11c62
Lap_Javi-Local <javixxmc@gmail.com>
2023-08-28 08:06:07 -0600

    --| Implementation of useEffect |--

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,34 @@
+import { useEffect, useState } from "react";
 import { getGifs } from "./helpers/getGifs"
 
 export const GifGrid = ({ category }) => {
-    getGifs( category );
+
+    const [counter, setCounter] = useState(10);
+
+    useEffect(() => {
+        getGifs( category );
+    }, []);
 
     return (
         <>
             <h3>{category}</h3>
+            <h5>{counter}</h5>
+            <button onClick={()=> setCounter(counter + 1)}>+ 1</button>
         </>
     )
 }
 
 /**
  * Este componente nos ayuda a renderizar las imagenes de tipo gif
- * segun la categoria de gifs
+ * segun la categoria de gifs, sin embargo cada vez que el componente 
+ * tiene un cambio y/o es invocado, React vuelve a dibujar dicho componente
+ * haciendo que el llamado de la función getGifs, se vuelva a ejecutar.
+ * 
+ * --|useEfect|--
+ * Con el useEfect, podemos renderizar la función getGifs solo una vez, sin importar que
+ * nuestro componente se actualice, por eso el segundo argumento del useEffect es un
+ * juego de corchetes vacios.
+ * A pesar de hacer uso del useEffect, se sigue disparando 2 veces cada petición,
+ * esto solo pasa en el entorno de desarrollo, esto se debe a la etiqueta React.StrictMode
+ * que se encuentra en el archivo principal.
  */
--- src/components/helpers/getGifs.js
+++ src/components/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async( category ) =>{
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=rTvAjGOs7Q5XZLlnYmO9PEP0tZTAJbLf&q=${ category }&limit=20`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=rTvAjGOs7Q5XZLlnYmO9PEP0tZTAJbLf&q=${ category }&limit=10`
     const resp = await fetch( url );
     const {data}= await resp.json();
 
@@ -8,7 +8,7 @@ export const getGifs = async( category ) =>{
         title: img.title,
         url: img.images.downsized_medium.url
     }));
-    console.log(gifs);
+    //console.log(gifs);
     return gifs;
 }
 
