cf94f6290ec1ad677c2e7ed61630ade658512c7a
Lap_Javi-Local <javixxmc@gmail.com>
2023-09-14 12:46:02 -0600

    --| New test and Update Readme file |--

---
+++ test/hooks/useFetchGifs.test.js
@@ -0,0 +1,13 @@
+import { renderHook } from "@testing-library/react";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+describe('Pruebas en hook useFetchGifs', () => {
+    test('debe de regresar el estado inicial', () => {
+        
+        const { result } = renderHook( () => useFetchGifs('One Punch') );
+        const { images, isLoading } = result.current;
+
+        expect(images.length).toBe(0);
+        expect( isLoading ).toBeTruthy();
+    });
+});
